0c3c10abcb7508ecf8c4f2a5bf95419ed52908cc
“ClarissaWaldron” <clarissa509@hotmail.com>
2019-11-07 12:03:32 +0000

    test config

--- src/Tvshowinfo.js
+++ src/Tvshowinfo.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function Tvshowinfo() {
     return (
         <div>
-            <h1> Here are some details about the shows.</h1>
+            <h1> Here are some details about the show.</h1>
             <Link to='/'>Back to homepage</Link>
         </div>
     );
