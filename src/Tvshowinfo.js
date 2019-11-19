88039fcea961c4a16e7963298c0b1e930b0c3251
“ClarissaWaldron” <clarissa509@hotmail.com>
2019-11-19 09:14:54 +0000

    introduce state componenet

--- src/Tvshowinfo.js
+++ src/Tvshowinfo.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 
-export default function Tvshowinfo() {
-    return (
-        <div>
-            <h1> Here are some details about the show.</h1>
+export default class Tvshowinfo extends React.Component {
+
+    constructor () {
+        super();
+        this.state = {
+            welcomeMessage: 'Here are some details about the show.'
+        };
+    }
+
+    componentDidMount() {
+        setTimeout(() => {
+            this.setState ({
+                welcomeMessage: 'Coming soon'
+            });
+        }, 3000);
+    }
+
+    render () {
+        return (
+            <div>
+            <h1> {this.state.welcomeMessage}</h1>
             <Link to='/'>Back to homepage</Link>
         </div>
     );
+    }
 }
